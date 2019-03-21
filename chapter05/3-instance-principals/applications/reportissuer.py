ecfd410a077b8b5f80caf8b7768f3dd39cbb2de7
Michal <mtjakobczyk@gmail.com>
2019-03-21 12:22:57 +0100

    Chapter 5 - fixing issuer

--- chapter05/3-instance-principals/applications/reportissuer.py
+++ chapter05/3-instance-principals/applications/reportissuer.py
@@ -14,8 +14,8 @@ def prepare_report_entries(storage_namespace, bucket_name, object_prefix, client
         report_entries.append(entry)
     return report_entries
 
-def upload_report(report_entry_list, storage_namespace, bucket_name, object_name, client):
-    tmp_report_filename = 'bucket_report.'+str(uuid.uuid4())+'.txt'
+def upload_report(report_entry_list, tmp_directory, storage_namespace, bucket_name, object_name, client):
+    tmp_report_filename = tmp_directory+'/bucket_report.'+str(uuid.uuid4())+'.txt'
     with open(tmp_report_filename, 'w') as stream:
         for entry in report_entry_list:
             stream.write(entry+'\n')
@@ -30,12 +30,13 @@ if __name__ == '__main__':
     bucket_name = os.environ['APP_BUCKET_NAME']
     summary_object_name = os.environ['APP_OBJECT_NAME']
     object_prefix = os.environ['APP_OBJECT_PREFIX']
-    polling_interval_seconds = os.environ['APP_POLLING_INTERVAL_SECONDS']
+    polling_interval_seconds = int(os.environ['APP_POLLING_INTERVAL_SECONDS'])
+    tmp_directory = os.environ['HOME']
     while True:
         print('Creating a report')
         signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
         client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
         storage_namespace = client.get_namespace().data
         report_entry_list = prepare_report_entries(storage_namespace, bucket_name, object_prefix, client)
-        upload_report(report_entry_list, storage_namespace, bucket_name, summary_object_name, client)
+        upload_report(report_entry_list, tmp_directory, storage_namespace, bucket_name, summary_object_name, client)
         time.sleep(polling_interval_seconds)
