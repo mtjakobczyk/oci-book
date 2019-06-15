53f453c41ed659efedeee81c9a86d4d49ab30ee8
Michal <mtjakobczyk@gmail.com>
2019-06-15 16:52:19 +0200

    Chapter 3 - SDK - minor fixes

--- chapter03/codeSnippets.sh
+++ chapter03/codeSnippets.sh
@@ -60,7 +60,7 @@ source ocidev/bin/activate
 python3
 ### python
 import oci
-config = oci.config.from_file(“~/.oci/config”,“DEFAULT”)
+config = oci.config.from_file("~/.oci/config","DEFAULT")
 identity = oci.identity.IdentityClient(config)
 ads_list = identity.list_availability_domains(config['tenancy']).data
 for ad in ads_list:
