86159947f7b594e4494fbadb71ab3da2fbb38e03
Michal <mtjakobczyk@gmail.com>
2019-06-15 18:03:32 +0200

    Chapter 3 - CLI - code snippets

--- chapter03/codeSnippets.sh
+++ chapter03/codeSnippets.sh
@@ -84,3 +84,65 @@ vcn.delete_vcn(response.data.id)
 quit()
 # bash
 deactivate
+
+
+# SECTION: CLI
+
+## Install OCI CLI
+### bash
+bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)"
+
+## Explore OCI CLI virtual environment
+### bash
+oci --version
+head -n 1 `which oci`
+cd ~/lib/oracle-cli/
+source bin/activate
+pip freeze | grep oci
+deactivate
+
+## Use CLI to query for Oracle-provided Ubuntu image names
+### bash
+cat ~/.oci/config | grep tenancy
+tenancy_ocid=put-here-tenancy-ocid
+oci compute image list --compartment-id $tenancy_ocid --operating-system "Canonical Ubuntu" --output table --query "data [*].{Image:\"display-name\"}"
+
+## Use predefined CLI queries to list specific versions of Oracle-provided Ubuntu image names
+### bash
+touch ~/.oci/oci_cli_rc
+##### Edit oci_cli_rc configuration file based on 2-sdk/oci_cli_rc file
+oci compute image list --operating-system "Canonical Ubuntu" --output table --query query://list_ubuntu_1804
+
+## Check current default compartment for CLI commands
+### bash
+oci iam compartment get --output table --query "data.{CompartmentName:\"name\"}"
+
+## Create VCN, Internet Gateway, Routing Table, Subnet and Compute Instance using CLI
+### bash
+vcn_ocid=`oci network vcn create --cidr-block 192.168.3.0/24 --display-name cli-vcn --query "data.id" | tr -d '"'`
+echo $vcn_ocid
+igw_ocid=`oci network internet-gateway create --vcn-id $vcn_ocid --display-name cli-igw --is-enabled true --query "data.id" | tr -d '"'`
+echo $igw_ocid
+route_rules="[{\"cidrBlock\":\"0.0.0.0/0\", \"networkEntityId\":\"$igw_ocid\"}]"
+rt_ocid=`oci network route-table create --vcn-id $vcn_ocid --display-name cli-rt --route-rules "$route_rules" --query "data.id" | tr -d '"'`
+echo $rt_ocid
+ad1=`oci iam availability-domain list --query data[0].name | tr -d '"'`
+echo $ad1
+subnet_ocid=`oci network subnet create --vcn-id $vcn_ocid --display-name cli-vcn --cidr-block "192.168.3.0/30" --prohibit-public-ip-on-vnic false --availability-domain $ad1 --route-table-id $rt_ocid --query data.id | tr -d '"'`
+echo $subnet_ocid
+image_ocid=`oci compute image list --operating-system "CentOS" --operating-system-version 7 --sort-by TIMECREATED --query data[0].id | tr -d '"'`
+echo $image_ocid
+vm_ocid=`oci compute instance launch --display-name cli-vm --availability-domain "$ad1" --subnet-id "$subnet_ocid" --private-ip 192.168.3.2 --image-id "$image_ocid" --shape VM.Standard2.1 --ssh-authorized-keys-file ~/.ssh/oci_id_rsa.pub --wait-for-state RUNNING --query data.id | tr -d '"'`
+echo $vm_ocid
+
+## Listing the public IP of the compute instance using CLI
+### bash
+oci compute instance list-vnics --instance-id "$vm_ocid" --query data[*].\"public-ip\"
+
+## Terminating Compute Instance, Subnet, Internet Gateway, Routing Table and VCN using CLI
+### bash
+oci compute instance terminate --instance-id $vm_ocid --wait-for-state TERMINATED
+oci network subnet delete --subnet-id $subnet_ocid --wait-for-state TERMINATED
+oci network route-table delete --rt-id $rt_ocid --wait-for-state TERMINATED
+oci network internet-gateway delete --ig-id $igw_ocid --wait-for-state TERMINATED
+oci network vcn delete --vcn-id $vcn_ocid 
