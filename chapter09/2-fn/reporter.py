d8b15049854b077e71539a860cd8cb5b5ec941ff
Michal <mtjakobczyk@gmail.com>
2019-09-21 23:55:55 +0200

    Chapter 9 - events

---
+++ chapter09/2-fn/reporter.py
@@ -0,0 +1,82 @@
+import io
+import json
+import uuid
+import oci
+
+from fdk import response
+from io import StringIO
+
+
+def extract_object_name(data: io.BytesIO):
+    data_bytes = data.getvalue()
+    data_json = json.loads(data_bytes)
+    object_name = data_json.get("object_name")
+    return object_name
+
+
+def load_object_content(client, storage_namespace, bucket_name, object_name):
+    res = client.get_object(storage_namespace, bucket_name, object_name)
+    # Response uses oci._vendor.urllib3.response.HTTPResponse
+    httpresponse = res.data.raw
+    data_bytes = b''
+    for chunk in httpresponse.stream(4096):
+        data_bytes += chunk
+    return data_bytes.decode('UTF-8')
+
+
+def process_city_attendance_data(object_content_str):
+    city_attendance_dict = {}
+    for line in object_content_str.splitlines():
+        line_list = line.split(',')
+        city = line_list[1]
+        if city_attendance_dict.get(city) is None:
+            city_attendance_dict[city] = 0
+        attendance = int(line_list[2])
+        city_attendance_dict[city] += attendance
+    return city_attendance_dict
+
+
+def put_city_attendance_object(client, storage_namespace, bucket_name, object_name, city_attendance_dict):
+    object_content_stream = StringIO(str(city_attendance_dict))
+    rsp = client.put_object(storage_namespace, bucket_name, object_name, object_content_stream)
+    return rsp
+
+
+def prepare_function_response(ctx, res_dict, headers_dict):
+    res_str = json.dumps(res_dict)
+    rsp = response.Response(ctx, response_data=res_str, headers=headers_dict)
+    return rsp
+
+
+def handler(ctx, data: io.BytesIO=None):
+    res_dict = {}
+    headers_dict={"Content-Type": "application/json"}
+
+    if data is None or not len(data.getvalue())>0:
+        res_dict["message"] = "no data received"
+        return prepare_response(ctx, res_dict, headers_dict)
+
+    try:
+        # Process input
+        bucket_name = "reports"
+        object_name = extract_object_name(data)
+
+        # Authenticate the function instance as an instance principal
+        signer = oci.auth.signers.get_resource_principals_signer()
+        client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
+        storage_namespace = client.get_namespace().data
+
+        object_content_str = load_object_content(client, storage_namespace, bucket_name, object_name)
+        city_attendance_dict = process_city_attendance_data(object_content_str)
+
+        processed_object_name = object_name.replace('.csv','.processed.csv')
+        put_response = put_city_attendance_object(client, storage_namespace, bucket_name, processed_object_name, city_attendance_dict)
+
+        res_dict["put_response"] = str(put_response)
+        res_dict["object_content"] = str(city_attendance_dict)
+        res_dict["result"] = "success"
+    except (Exception, ValueError) as ex:
+        res_dict["result"] = "error"
+        res_dict["reason"] = str(ex)
+
+    return prepare_function_response(ctx, res_dict, headers_dict)
--- chapter09/codeSnippets.sh
+++ chapter09/codeSnippets.sh
@@ -113,8 +113,10 @@ oci iam policy create --name sandbox-users-functions-policy --description "Funct
 ## Create context, OCI config
 ### bash (on cloud-based VM)
 fn create context sandbox-user-fra-oci --provider oracle
+# Edit the ~/.fn/contexts/sandbox-user-fra-oci.yaml file as described in the book
 mkdir ~/.oci
 touch ~/.oci/config
+# Edit the ~/.oci/config file as described in the book
 
 ## Upload SANDBOX-USER key, config and connect to the Dev Machine
 ### bash
@@ -136,8 +138,11 @@ fn list apps
 # SECTION: Serverless > Oracle Functions > Deploy function
 
 ## Create application in Oracle Functions
+### bash
+cd ~/git/oci-book/chapter09/1-infrastructure
+terraform output functions_subnet_ocid
 ### bash (on cloud-based VM)
-FN_SUBNET_ID=`terraform output functions_subnet_ocid`
+FN_SUBNET_ID={put-here-subnet-ocid}
 fn create app uuidcloudapp --annotation oracle.com/oci/subnetIds="[\"$FN_SUBNET_ID\"]"
 
 ## Login to OCIR
@@ -149,6 +154,7 @@ docker login -u $OCI_TENANCY/$OCI_USER $OCIR_REGION.ocir.io
 
 ## Deploy function manifest and push image to OCIR
 ### bash (on cloud-based VM)
+cd ~/uuidfn
 fn -v deploy --app uuidcloudapp --no-bump
 
 ## Inspect Oracle Function
@@ -160,7 +166,69 @@ fn inspect app uuidcloudapp
 ### bash (on cloud-based VM)
 fn invoke uuidcloudapp uuidfn
 echo -n '{ "client_name": "some_app"  }' | fn invoke uuidcloudapp uuidfn --content-type application/json
+fn invoke uuidcloudapp uuidfn
+fn invoke uuidcloudapp uuidfn
 
 ## Listing Oracle Function endpoint
 ### bash (on cloud-based VM)
 fn inspect function uuidcloudapp uuidfn | jq -r '.annotations."fnproject.io/fn/invokeEndpoint"'
+
+
+# SECTION: Serverless > Events > Events as function triggers
+
+# Create Object Storage bucket for reports
+### bash
+oci os bucket create --name reports --profile SANDBOX-ADMIN
+cd ~/git/oci-book/chapter09/4-events/reports
+oci os object put -bn reports --file customer_attendance.20190922.csv --profile SANDBOX-USER
+oci os object put -bn reports --file customer_attendance.20190923.csv --profile SANDBOX-USER
+
+## Create tag key inside the existing tag namespace
+### bash
+TAG_NAMESPACE_OCID=`oci iam tag-namespace list --query "data[?name=='test-projects'] | [0].id" --raw-output`
+echo $TAG_NAMESPACE_OCID
+oci iam tag create --tag-namespace-id $TAG_NAMESPACE_OCID --name reports --description "Reports project" --profile SANDBOX-ADMIN
+
+# Dynamic Group for tagged functions
+### bash
+echo $TENANCY_OCID
+MATCHING_RULE="ALL {resource.type = 'fnfunc', tag.test-projects.reports.value}"
+oci iam dynamic-group create --name reporting-functions --description "Functions related to the reporting project" --matching-rule $MATCHING_RULE -c $TENANCY_OCID
+
+
+## IAM Policy statement
+### bash
+cd ~/git/oci-book/chapter09/4-events/policies
+oci iam policy create --name sandbox-users-storage-reports-policy --statements file://sandbox-users.policies.storage-reports.json --description "Storage-related (reports) policy for regular Sandbox users" --profile SANDBOX-ADMIN
+oci iam policy create --name functions-storage-reports-policy --statements file://functions.policies.storage-reports.json --description "Storage-related (reports) policy for tagged functions" --profile SANDBOX-ADMIN
+
+
+## Create application in Oracle Functions
+### bash (on cloud-based VM)
+cd ~
+fn init --runtime python reportingfn
+cp ~/functions/reportingfn.py ~/reportingfn/func.py
+fn create app reportingapp --annotation oracle.com/oci/subnetIds="[\"$FN_SUBNET_ID\"]"
+cd reportingfn/
+fn -v deploy --app reportingapp --no-bump
+
+# Test failed (no tag on function)
+### bash (on cloud-based VM)
+echo -n '{ "object_name": "report1.txt", "object_content": "Some Text"  }' | fn invoke reportingapp reportingfn --content-type application/json
+# No object found
+
+## Tag function with the defined tag key
+### bash
+FN_APP_OCID=`oci fn application list --query "data[?\"display-name\" == 'reportingapp'] | [0].id" --raw-output`
+echo $FN_APP_OCID
+FN_FUN_OCID=`oci fn function list --application-id $FN_APP_OCID --query "data[?\"display-name\" == 'reportingfn'] | [0].id" --raw-output`
+echo $FN_FUN_OCID
+oci fn function update --function-id $FN_FUN_OCID --defined-tags '{ "test-projects": {"reports": ""} }'
+
+# Test failed (no tag on function)
+### bash (on cloud-based VM)
+echo -n '{ "object_name": "report1.txt", "object_content": "Some Text"  }' | fn invoke reportingapp reportingfn --content-type application/json
+# No object found
+
+## Define Oracle Event
+### bash
