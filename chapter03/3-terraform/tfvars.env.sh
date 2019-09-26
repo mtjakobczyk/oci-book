aff6f92ac44842c84143703e3596d72cb6d04923
Michal Jakobczyk <mtjakobczyk@gmail.com>
2019-09-26 13:32:08 +0200

    Update tfvars.env.sh

--- chapter03/3-terraform/tfvars.env.sh
+++ chapter03/3-terraform/tfvars.env.sh
@@ -1,9 +1,9 @@
 # Terraform
-export TF_VAR_tenancy_ocid=put-here-tenancy-ocid
-export TF_VAR_user_ocid=put-here-user-ocid
-export TF_VAR_fingerprint=put-here-key-fingerprint
-export TF_VAR_region=put-here-region-name
-export TF_VAR_private_key_path=put-here-path-to-private-key
+export TF_VAR_tenancy_ocid=<put-here-tenancy-ocid>
+export TF_VAR_user_ocid=<put-here-user-ocid>
+export TF_VAR_fingerprint=<put-here-api-signing-key-fingerprint>
+export TF_VAR_region=<put-here-region-identifier>
+export TF_VAR_private_key_path=<put-here-path-to-private-key>
 
-export TF_VAR_private_key_password=put-here-private-key-password
-export TF_VAR_compartment_ocid=put-here-compartment-ocid
+export TF_VAR_private_key_password=<put-here-private-key-password>
+export TF_VAR_compartment_ocid=<put-here-compartment-ocid>
