a2c87ae04fb6b6301821dc61010c8ca9c21e0f31
Michal Jakobczyk <mtjakobczyk@gmail.com>
2019-10-02 18:19:13 +0200

    Update modules.tf

--- chapter08/1-devmachine/modules.tf
+++ chapter08/1-devmachine/modules.tf
@@ -6,6 +6,7 @@ data "oci_core_images" "centos_image" {
   compartment_id = var.compartment_ocid
   operating_system = "CentOS"
   operating_system_version = 7
+  shape = "VM.Standard2.1"
 }
 module "devmachine" {
   source           = "./devmachine"
