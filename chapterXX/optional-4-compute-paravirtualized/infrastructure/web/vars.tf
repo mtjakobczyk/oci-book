3bfe680bfe09c2ee910aa2174ba51f31f6ca19e7
Michal <mtjakobczyk@gmail.com>
2019-10-03 18:19:28 +0200

    Deleting unnamed chapter

--- chapterXX/optional-4-compute-paravirtualized/infrastructure/web/vars.tf
+++
@@ -1,9 +0,0 @@
-# bastion / variables
-## Module Input Variables
-variable "compartment_ocid" {}
-variable "vcn_ocid" {}
-variable "vcn_igw_ocid" {}
-variable "vcn_cidr" { }
-variable "vcn_subnet_cidr" { }
-variable "ad" { }
-variable "image_ocid" {}
